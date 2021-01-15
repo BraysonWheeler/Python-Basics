68c19fb39dd27bb81a8b35848a8ca56344a79d69
Logan Wheeler <35555839+BraysonWheeler@users.noreply.github.com>
2021-01-15 10:41:46 -0600

    Create Vars.py

---
+++ Vars.py
@@ -0,0 +1,19 @@
+#LEH Python 3 syntax
+a = 5
+b = 3
+c = a + b
+print (c)
+print(a/b)
+print(int(a/b)) #can be float int etc...
+c = "Hello"
+print(c)
+c = 5
+#Used in backdoor
+string = "This number is: %d" %c # % refers to where to place the c into
+print (string)
+strr = "Hello";
+strr1 = "Our string is: %s" %strr 
+print (strr1)
+b = 3.0
+string2 = "Our number is: %f" %b
+print(string2)
