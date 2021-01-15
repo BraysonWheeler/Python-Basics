629a945d13649a765a1e77df0004ddda4612b49b
Logan Wheeler <35555839+BraysonWheeler@users.noreply.github.com>
2021-01-15 11:26:29 -0600

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,12 @@
+string_list = ["hello","World","What","is","Happening"]
+print (string_list)
+print (string_list[1])
+print (string_list[1:])
+print (string_list[:2])
+
+
+string_list.remove("What")
+print (string_list)
+
+string_list.append("asd")
+print (string_list)
